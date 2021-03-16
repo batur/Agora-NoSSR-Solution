e8a5ad4630ec17164d955148dba2d39a3c22af79
cosakin <halidbaturakkurt@gmail.com>
2021-03-16 18:20:31 +0300

    Basic Template

---
+++ src/componenet/Dynamic/meeting.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AgoraRTC from "agora-rtc-sdk-ng";
+
+function Meeting() {
+  return (
+    <div>
+      <h1>No SSR</h1>
+    </div>
+  );
+}
+
+export default Meeting;
